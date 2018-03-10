b6cd3e4ab637b3bd2fd1c8679e29e73e2e37e4cc
dolly <timohakt@yandex.ru>
2018-03-10 10:29:45 +0300

    Change names of variables

--- duplicates.py
+++ duplicates.py
@@ -2,22 +2,23 @@ import os
 import sys
 
 
-def get_files_by_size(folder):
-    files_by_size = {}
+def get_files_dict(folder):
+    files_dict = {}
     for dir_path, dir_name, file_names in os.walk(folder):
         for file_name in file_names:
             file_path = os.path.join(dir_path, file_name)
             file_info = os.stat(file_path)
             file_size = file_info.st_size
-            files_by_size.setdefault((file_name, file_size), []).append(file_path)
-    return files_by_size
+            files_dict.setdefault((file_name, file_size), []).append(file_path)
+    return files_dict
 
 
-def get_duplicates(files_by_size):
+def get_duplicates(files_dict):
     duplicates_in_dir = []
-    for file_size, duplicates in files_by_size.items():
+    for file_size, paths in files_dict.items():
         if len(duplicates) > 1:
-            duplicates_in_dir.extend(duplicates[1:])  # The first is original
+            # The first is path to  original file
+            duplicates_in_dir.extend(paths[1:])
     return duplicates_in_dir
 
 
@@ -33,8 +34,8 @@ if __name__ == "__main__":
 
     src_dirname = sys.argv[1]
 
-    files_by_size = get_files_by_size(src_dirname)
-    duplicates = get_duplicates(files_by_size)
+    files_dict = get_files_dict(src_dirname)
+    duplicates = get_duplicates(files_dict)
 
     print('Found duplicates in a directory "{}":'.format(src_dirname))
     for duplicate in duplicates:
